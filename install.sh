8a3b846866dc48040ba9b8fe264e6f8e35f489ca
Diaa <a2diaa@uwaterloo.ca>
2024-09-04 02:41:06 +0300

    update script changes

--- install.sh
+++ install.sh
@@ -13,29 +13,29 @@ export CUDA
 conda install python=3.9 cmake ninja openssl
 
 pip install --upgrade pip
+#pip install torch torchvision torchaudio xformers || exit 1
 pip install --upgrade setuptools
 
 
 echo "=> Installing vllm..."
 pip install submodules/vllm || exit 1
 
-
 # Install specific versions of packages for VLLM compatibility
 pip install lingua-language-detector tiktoken transformers scikit-learn nltk pyinflect accelerate openai textattack pandas dacite dahuffman argostranslate dill mauve-text faiss-gpu sacrebleu sacremoses psutil pyext
+pip install xformers || exit 1
 
 #pip install starlette==0.27.0
 
 echo "=> Installing APPS..."
 pip install -e submodules/apps || exit 1
 
+echo "=> Installing watermark-benchmark..."
+python setup.py install || exit 1
+
+pip install --upgrade openai httpcore
+
 echo "=> Installing cpp-hash..."
 cd submodules/cpp-hash || exit 1
 python setup.py install || exit 1
 
-pip install torch torchvision torchaudio xformers || exit 1
-
-echo "=> Installing watermark-benchmark..."
 cd ../..
-python setup.py install || exit 1
-
-pip install --upgrade openai httpcore
